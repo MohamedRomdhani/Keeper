6e2c8b7a9e96c4313c0f14ea6fdb359f4959c5c8
MohamedRomdhani <65048600+MohamedRomdhani@users.noreply.github.com>
2021-09-23 13:35:47 +0200

    Keeper-App-Version 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+var a = new Date().getFullYear();
+
+function Footer(){
+    return (
+        <footer>
+            <p>Copyright {a}</p>
+        </footer>
+    )
+
+
+
+}
+
+export default Footer;
